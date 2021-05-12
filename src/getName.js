f5111e36e9c13305a36771007745c3ed5f8e58f5
dmjcomdem <dmjcomdem.style@gmail.com>
2021-05-12 18:39:52 +0500

    Add test for getName.js

--- src/getName.js
+++ src/getName.js
@@ -4,9 +4,9 @@ export const getName = (url, type = 'file') => {
   if (!url) return '';
 
   const { host, pathname } = new URL(url);
-  const urlPath = `${host}${pathname}`;
-  const extname = path.extname(urlPath) || '.html';
-  const fileName = urlPath.replace(/[\W]/gi, '-');
+  const urlPath = `${host}${pathname === '/' ? '' : pathname}`;
+  const extname = path.extname(pathname) || '.html';
+  const fileName = urlPath.replace(extname, '').replace(/[\W]/gi, '-');
   const postfix = type === 'file' ? extname : '_files';
 
   return `${fileName}${postfix}`;
