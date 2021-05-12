c2dbf17409cd015033d088d2ee3017b859f1b48a
dmjcomdem <dmjcomdem.style@gmail.com>
2021-05-13 02:25:37 +0500

    Add __fixtures__ and debug package

--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
 import fs from 'fs/promises';
 import path from 'path';
+import debug from 'debug';
 
 import { getName } from './getName';
 import { request } from './request';
 import { getResource } from './getResource';
 
-export const loader = async (url, folder) => {
+const logger = debug('page-loader');
+
+export const loader = async (url, folder, log = logger) => {
+  log('url is empty');
   if (!url) return '';
 
-  const fileName = getName(url);
-  const folderName = getName(url, 'folder');
-  const filePath = path.resolve(__dirname, folder, fileName);
-  const folderPath = path.resolve(__dirname, folder, folderName);
+  try {
+    const fileName = getName(url);
+    const folderName = getName(url, 'folder');
 
-  const htmlData = await request(url, { responseType: 'text' });
-  const { data, links } = getResource(htmlData, url);
+    const filePath = path.resolve(__dirname, folder, fileName);
+    const folderPath = path.resolve(__dirname, folder, folderName);
 
-  await fs.mkdir(folderPath);
-  await fs.writeFile(filePath, data);
+    log(`fetch ${url}`);
+    const htmlData = await request(url, { responseType: 'text' });
 
-  const promises = links.map(async ({ href, name }) => {
-    const response = await request(href, { responseType: 'arraybuffer' });
-    await fs.writeFile(`${folderPath}/${name}`, response);
-  });
+    log(`get resources page`);
+    const { data, links } = getResource(htmlData, url);
 
-  await Promise.all(promises);
+    log(`create directory ${folderPath}`);
+    await fs.mkdir(folderPath, { recursive: true });
 
-  return filePath;
+    log(`write file ${filePath}`);
+    await fs.writeFile(filePath, data);
+
+    const promises = links.map(async ({ href, name }) => {
+      const response = await request(href, { responseType: 'arraybuffer' });
+      await fs.writeFile(`${folderPath}/${name}`, response);
+      log(`✔ fetch and write resource ${href}`);
+    });
+
+    await Promise.all(promises);
+
+    return filePath;
+  } catch (error) {
+    log(`error: ${error}`);
+    console.error(error);
+  }
 };
