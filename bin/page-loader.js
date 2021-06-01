17cbe9ae5ce80de733db5f90535b1501f36fa469
dmjcomdem <d.romanov@zdravcity.ru>
2021-06-02 01:04:47 +0500

    Check commander init

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import pageLoader from '../../page-loader-cli-0ca243661aae04e38bfe395142e8b32e49b8d56f/src';
+import pageLoader from '../src/index';
 
 const program = new Command();
 program.option('-o, --output <type>', 'path to save file', process.cwd());
@@ -8,4 +8,5 @@ program.option('-o, --output <type>', 'path to save file', process.cwd());
 program.parse(process.argv);
 const options = program.opts();
 pageLoader(program.args[0], options.output)
-  .then(() => process.exit(0)).catch(() => process.exit(1));
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1));
