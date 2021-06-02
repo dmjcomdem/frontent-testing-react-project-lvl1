e5eb37c451fc76c16f816fa52ec99c53087da124
dmjcomdem <d.romanov@zdravcity.ru>
2021-06-02 10:49:28 +0500

    Remove methods isLocalURL

--- src/getResource.js
+++ src/getResource.js
@@ -9,28 +9,25 @@ const tagsMap = {
   script: 'src',
 };
 
-const isLocalURL = (url, origin) => {
-  if (url === undefined) {
-    return false;
-  }
-  return new URL(origin).origin === new URL(url, origin).origin;
-};
-
 const getResource = (data, url) => {
   const links = [];
   const $ = cheerio.load(data);
   const folder = getName(url, 'folder');
+  const { origin } = new URL(url);
 
   Object.entries(tagsMap).forEach(([tag, attr]) => {
     $(tag).each((_, el) => {
       const value = $(el).attr(attr);
+
       if (!value) return;
 
-      if (!isLocalURL(value, url)) {
+      const link = new URL(value, origin);
+
+      if (link.origin !== origin) {
         return;
       }
 
-      const href = new URL(value, url).toString();
+      const href = link.toString();
       const name = getName(href);
       console.log(`file name: ${chalk.cyan.bold(name)}`);
       const path = `${folder}/${name}`;
